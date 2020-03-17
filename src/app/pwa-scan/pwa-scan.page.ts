6244723590ef619179f41f19a12eaf8abe5a4498
Malik-shahzadAli <shahzadali@dedevtech.com>
2020-03-17 11:41:26 +0500

    new branch

--- src/app/pwa-scan/pwa-scan.page.ts
+++ src/app/pwa-scan/pwa-scan.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Barcode, BarcodePicker, Camera, CameraAccess, CameraSettings, ScanResult, ScanSettings } from "scandit-sdk";
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-pwa-scan',
   templateUrl: './pwa-scan.page.html',
@@ -8,13 +8,14 @@ import { Barcode, BarcodePicker, Camera, CameraAccess, CameraSettings, ScanResul
 })
 export class PwaScanPage implements OnInit {
   public settings = new ScanSettings({ enabledSymbologies: [Barcode.Symbology.PDF417] });
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
   onScan(event) {
-    console.log(event);
+    // console.log(event);
     // this.getErrorTost(event);
+    this.router.navigate(['/primary-name']);
   }
   onInit() {
     console.log('on Init');
